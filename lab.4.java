b10d3468c45e2a30ea609bfdca88ea0c5c13584e
orxanusubaliyev <127599893+orxanusubaliyev@users.noreply.github.com>
2023-05-10 17:58:12 +0400

    Add files via upload

---
+++ lab.4.java
@@ -0,0 +1,7 @@
+let eni = parseFloat(prompt("Enini daxil edin:"));
+let uzun = parseFloat(prompt("Uzununu daxil edin:"));
+
+let sahe = eni * uzun;
+let perimetri = 2 * (eni + uzun);
+
+alert("duzbucaqlinin sahesi: " + sahe + "\n:duzbucaqlinin perimetri "  + perimetri);
